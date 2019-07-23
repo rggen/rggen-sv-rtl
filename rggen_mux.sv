a1129392cdfc37691a60e1ef5604acabe66af704
taichi <taichi730@gmail.com>
2019-07-23 15:18:34 +0900

    changed mux implementation

--- rggen_mux.sv
+++ rggen_mux.sv
@@ -2,22 +2,31 @@ interface rggen_mux #(
   parameter int WIDTH   = 8,
   parameter int ENTRIES = 2
 )();
+  localparam  int INDEX_WIDTH = (ENTRIES >= 2) ? $clog2(ENTRIES) : 1;
+
   function automatic logic [WIDTH-1:0] mux(
     logic [ENTRIES-1:0] select,
     logic [WIDTH-1:0]   value_in[ENTRIES]
   );
-    logic [WIDTH-1:0] out;
-
-    if (ENTRIES > 1) begin
-      out = '0;
-      for (int i = 0;i < ENTRIES;++i) begin
-        out = out | ({WIDTH{select[i]}} & value_in[i]);
-      end
+    if (ENTRIES >= 2) begin
+      return value_in[get_index(select)];
     end
     else begin
-      out = value_in[0];
+      return value_in[0];
     end
+  endfunction
 
-    return out;
+  function automatic logic [INDEX_WIDTH-1:0] get_index(
+    logic [ENTRIES-1:0] select
+  );
+    logic [INDEX_WIDTH-1:0] index;
+    for (int i = 0;i < INDEX_WIDTH;++i) begin
+      logic [ENTRIES-1:0] temp;
+      for (int j = 0;j < ENTRIES;++j) begin
+        temp[j] = select[j] & j[i];
+      end
+      index[i]  = |temp;
+    end
+    return index;
   endfunction
 endinterface
