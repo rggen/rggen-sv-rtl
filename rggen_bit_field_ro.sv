c899569456113e363a45d9ad90705aca3adfff92
taichi <taichi730@gmail.com>
2019-07-26 00:33:51 +0900

    removed clock and reset port

--- rggen_bit_field_ro.sv
+++ rggen_bit_field_ro.sv
@@ -1,8 +1,6 @@
 module rggen_bit_field_ro #(
   parameter int WIDTH = 8
 )(
-  input logic                   i_clk,
-  input logic                   i_rst_n,
   rggen_bit_field_if.bit_field  bit_field_if,
   input logic [WIDTH-1:0]       i_value
 );
