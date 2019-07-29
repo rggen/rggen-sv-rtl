a0d32acb7b8457f9aea4725bed70f4dafe2dbf8b
taichi <taichi730@gmail.com>
2019-07-29 16:08:07 +0900

    simplied backdoor access

--- rggen_backdoor_if.sv
+++ rggen_backdoor_if.sv
@@ -6,79 +6,81 @@ interface rggen_backdoor_if(
   input logic i_clk,
   input logic i_rst_n
 );
-  import  uvm_pkg::uvm_reg_data_logic_t;
+  import  uvm_pkg::uvm_reg_data_t;
 
-  typedef struct {
-    uvm_reg_data_logic_t  read_mask;
-    uvm_reg_data_logic_t  write_mask;
-    uvm_reg_data_logic_t  write_data;
-  } rggen_backdoor_access;
+  bit             valid;
+  uvm_reg_data_t  read_mask;
+  uvm_reg_data_t  write_mask;
+  uvm_reg_data_t  write_data;
+  uvm_reg_data_t  read_data;
 
-  logic                 valid;
-  uvm_reg_data_logic_t  read_mask;
-  uvm_reg_data_logic_t  write_mask;
-  uvm_reg_data_logic_t  write_data;
-  uvm_reg_data_logic_t  read_data;
-
-  mailbox backdoor_access_mailbox;
-  initial begin
-    backdoor_access_mailbox = new(1);
-  end
-
-  always_ff @(posedge i_clk, negedge i_rst_n) begin
-    if (!i_rst_n) begin
-      valid <= '0;
-    end
-    else if (backdoor_access_mailbox.num() > 0) begin
-      rggen_backdoor_access backdoor_access;
-      backdoor_access_mailbox.get(backdoor_access);
-      valid       <= '1;
-      read_mask   <= backdoor_access.read_mask;
-      write_mask  <= backdoor_access.write_mask;
-      write_data  <= backdoor_access.write_data;
-    end
-    else begin
-      valid <= '0;
-    end
-  end
-
-  clocking monitor_cb @(posedge i_clk);
-    input valid;
-    input read_mask;
-    input write_mask;
-    input write_data;
-    input read_data;
+  clocking backdoor_cb @(posedge i_clk);
+    output  valid;
+    output  read_mask;
+    output  write_mask;
+    output  write_data;
+    input   read_data;
   endclocking
 
+  event at_clock_edge;
+  always @(backdoor_cb) begin
+    ->at_clock_edge;
+  end
+
+  semaphore backdoor_access_lock;
+  initial begin
+    backdoor_access_lock  = new(1);
+  end
+
   task automatic backdoor_read(
-    input uvm_reg_data_logic_t  mask,
-    ref   uvm_reg_data_logic_t  data
+    input uvm_reg_data_t  mask,
+    ref   uvm_reg_data_t  data
   );
-    rggen_backdoor_access backdoor_access;
-    backdoor_access.read_mask   = mask;
-    backdoor_access.write_mask  = '0;
-    backdoor_access.write_data  = '0;
-    backdoor_access_mailbox.put(backdoor_access);
-    data  = get_read_data();
+    backdoor_access(0, mask, data);
   endtask
 
   task automatic backdoor_write(
-    uvm_reg_data_logic_t  mask,
-    uvm_reg_data_logic_t  data
+    uvm_reg_data_t  mask,
+    uvm_reg_data_t  data
   );
-    rggen_backdoor_access backdoor_access;
-    backdoor_access.read_mask   = '0;
-    backdoor_access.write_mask  = mask;
-    backdoor_access.write_data  = data;
-    backdoor_access_mailbox.put(backdoor_access);
+    backdoor_access(1, mask, data);
   endtask
 
-  function automatic uvm_reg_data_logic_t get_read_data();
-    return monitor_cb.read_data;
+  task automatic backdoor_access(
+    input bit                   write,
+    input uvm_reg_data_t  mask,
+    ref   uvm_reg_data_t  data
+  );
+    backdoor_access_lock.get(1);
+
+    if (!at_clock_edge.triggered) begin
+      @(backdoor_cb);
+    end
+
+    backdoor_cb.valid <= '1;
+    if (write) begin
+      backdoor_cb.read_mask   <= '0;
+      backdoor_cb.write_mask  <= mask;
+      backdoor_cb.write_data  <= data;
+    end
+    else begin
+      data  = get_read_data();
+      backdoor_cb.read_mask   <= mask;
+      backdoor_cb.write_mask  <= '0;
+    end
+
+    @(backdoor_cb);
+    backdoor_cb.valid <= '0;
+
+    backdoor_access_lock.put(1);
+  endtask
+
+  function automatic uvm_reg_data_t get_read_data();
+    return backdoor_cb.read_data;
   endfunction
 
   task automatic wait_for_change();
-    @(monitor_cb.read_data);
+    @(backdoor_cb.read_data);
   endtask
 endinterface
 `endif
