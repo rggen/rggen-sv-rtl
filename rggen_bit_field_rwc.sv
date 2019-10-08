a3f98a584cfa5cf6cad2a538f76f12a182ca6ecb
taichi <taichi730@gmail.com>
2019-10-08 12:58:36 +0900

    introduced WRITE_FIRST parameter

--- rggen_bit_field_rwc.sv
+++ rggen_bit_field_rwc.sv
@@ -1,6 +1,7 @@
 module rggen_bit_field_rwc #(
   parameter int             WIDTH         = 8,
-  parameter bit [WIDTH-1:0] INITIAL_VALUE = '0
+  parameter bit [WIDTH-1:0] INITIAL_VALUE = '0,
+  parameter bit             WRITE_FIRST   = 1
 )(
   input   logic                 i_clk,
   input   logic                 i_rst_n,
@@ -18,13 +19,39 @@ module rggen_bit_field_rwc #(
     if (!i_rst_n) begin
       value <= INITIAL_VALUE;
     end
-    else if (i_clear) begin
-      value <= INITIAL_VALUE;
-    end
-    else if (bit_field_if.valid) begin
-      value <=
-        (bit_field_if.write_data &   bit_field_if.write_mask ) |
-        (value                   & (~bit_field_if.write_mask));
+    else begin
+      value <= get_next_value();
     end
   end
+
+  function automatic logic [WIDTH-1:0] get_next_value();
+    logic [WIDTH-1:0] write_data;
+    logic [WIDTH-1:0] write_mask;
+    logic             write_access;
+    logic [1:0]       source_select;
+
+    write_data    = bit_field_if.write_data;
+    write_mask    = bit_field_if.write_mask;
+    write_access  = (bit_field_if.valid) ? |write_mask : '0;
+
+    if (WRITE_FIRST && write_mask) begin
+      source_select = {1'b0, 1'b1};
+    end
+    else if ((!WRITE_FIRST) && i_clear) begin
+      source_select = {1'b1, 1'b0};
+    end
+    else begin
+      source_select = {i_clear, write_access};
+    end
+
+    if (source_select[0]) begin
+      return (write_data & write_mask) | (value & (~write_mask));
+    end
+    else if (source_select[1]) begin
+      return INITIAL_VALUE;
+    end
+    else begin
+      return value;
+    end
+  endfunction
 endmodule
--- rggen_bit_field_rws.sv
+++ rggen_bit_field_rws.sv
@@ -1,6 +1,7 @@
 module rggen_bit_field_rws #(
   parameter int             WIDTH         = 8,
-  parameter bit [WIDTH-1:0] INITIAL_VALUE = '0
+  parameter bit [WIDTH-1:0] INITIAL_VALUE = '0,
+  parameter bit             WRITE_FIRST   = 1
 )(
   input   logic                 i_clk,
   input   logic                 i_rst_n,
@@ -19,13 +20,39 @@ module rggen_bit_field_rws #(
     if (!i_rst_n) begin
       value <= INITIAL_VALUE;
     end
-    else if (i_set) begin
-      value <= i_value;
-    end
-    else if (bit_field_if.valid) begin
-      value <=
-        (bit_field_if.write_data &   bit_field_if.write_mask ) |
-        (value                   & (~bit_field_if.write_mask));
+    else begin
+      value <= get_next_value();
     end
   end
+
+  function automatic logic [WIDTH-1:0] get_next_value();
+    logic [WIDTH-1:0] write_data;
+    logic [WIDTH-1:0] write_mask;
+    logic             write_access;
+    logic [1:0]       source_select;
+
+    write_data    = bit_field_if.write_data;
+    write_mask    = bit_field_if.write_mask;
+    write_access  = (bit_field_if.valid) ? |write_mask : '0;
+
+    if (WRITE_FIRST && write_access) begin
+      source_select = {1'b0, 1'b1};
+    end
+    else if ((!WRITE_FIRST) && i_set) begin
+      source_select = {1'b1, 1'b0};
+    end
+    else begin
+      source_select = {i_set, write_access};
+    end
+
+    if (source_select[0]) begin
+      return (write_data & write_mask) | (value & (~write_mask));
+    end
+    else if (source_select[1]) begin
+      return i_value;
+    end
+    else begin
+      return value;
+    end
+  endfunction
 endmodule
