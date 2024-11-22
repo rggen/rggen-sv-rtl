c357e196b6f48be62e7b6c1262441bbdd967df3b
Taichi Ishitani <taichi730@gmail.com>
2024-11-22 16:22:50 +0900

    change indirect index logic
    (refs: rggen/rggen#224)

--- rggen_indirect_register.sv
+++ rggen_indirect_register.sv
@@ -1,23 +1,25 @@
 module rggen_indirect_register #(
-  parameter bit                             READABLE              = 1,
-  parameter bit                             WRITABLE              = 1,
-  parameter int                             ADDRESS_WIDTH         = 8,
-  parameter bit [ADDRESS_WIDTH-1:0]         OFFSET_ADDRESS        = '0,
-  parameter int                             BUS_WIDTH             = 32,
-  parameter int                             DATA_WIDTH            = BUS_WIDTH,
-  parameter int                             VALUE_WIDTH           = BUS_WIDTH,
-  parameter int                             INDIRECT_INDEX_WIDTH  = 1,
-  parameter bit [INDIRECT_INDEX_WIDTH-1:0]  INDIRECT_INDEX_VALUE  = '0
+  parameter bit                     READABLE              = 1,
+  parameter bit                     WRITABLE              = 1,
+  parameter int                     ADDRESS_WIDTH         = 8,
+  parameter bit [ADDRESS_WIDTH-1:0] OFFSET_ADDRESS        = '0,
+  parameter int                     BUS_WIDTH             = 32,
+  parameter int                     DATA_WIDTH            = BUS_WIDTH,
+  parameter int                     VALUE_WIDTH           = BUS_WIDTH,
+  parameter int                     INDIRECT_MATCH_WIDTH  = 1
 )(
   input logic                             i_clk,
   input logic                             i_rst_n,
   rggen_register_if.register              register_if,
-  input logic [INDIRECT_INDEX_WIDTH-1:0]  i_indirect_index,
+  input logic [INDIRECT_MATCH_WIDTH-1:0]  i_indirect_match,
   rggen_bit_field_if.register             bit_field_if
 );
   logic index_matched;
 
-  assign  index_matched = (i_indirect_index == INDIRECT_INDEX_VALUE) ? '1 : '0;
+  always_comb begin
+    index_matched = i_indirect_match == '1;
+  end
+
   rggen_register_common #(
     .READABLE       (READABLE       ),
     .WRITABLE       (WRITABLE       ),
